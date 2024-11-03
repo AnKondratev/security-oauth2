e4997da422703ac2f7fbf4e3d53f0f7fa4e86456
AnKondratev <kondratev.an@inbox.ru>
2024-11-04 03:38:48 +0400

    -

--- src/main/java/na/kondratev/security_oauth2/config/SecurityConfig.java
+++ src/main/java/na/kondratev/security_oauth2/config/SecurityConfig.java
@@ -1,22 +1,18 @@
 package na.kondratev.security_oauth2.config;
 
 import na.kondratev.security_oauth2.service.SocialAppService;
-import org.hibernate.property.access.internal.PropertyAccessMapImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
@@ -35,23 +31,6 @@ public class SecurityConfig {
                         .requestMatchers("/user_access/**").hasRole("USER")
                         .anyRequest().authenticated()
                 )
-//
-//                .oauth2ResourceServer(config -> config.jwt(jwt -> {
-//                    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//                    jwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
-//                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter);
-//                    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//                    JwtGrantedAuthoritiesConverter customJwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//                    customJwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("groups");
-//                    customJwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
-//
-//                    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(token ->
-//                            Stream.concat(jwtGrantedAuthoritiesConverter.convert(token).stream(),
-//                                            customJwtGrantedAuthoritiesConverter.convert(token).stream())
-//                                    .toList());
-//                }))
-//
-//
                 .oauth2Login(oauth2Login -> oauth2Login
                         .loginPage("/login")
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
